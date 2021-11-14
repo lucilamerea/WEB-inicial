88aa9364f4145532e0492f48ce0c75e4f62b4817
lucilamerea <91205654+lucilamerea@users.noreply.github.com>
2021-11-14 09:45:29 -0300

    mensaje

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ const express= require('express');
 const app= express();
 const path = require('path');
 
+app.use(express.json());
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
 
-app.get("/index",(req,res)=>{
-    res.send('index')
-})
+app.get('/index',(req,res)=>{
+    res.render('index')
+    //res.send('index')
+});
+
+
+app.get('/index2',(req,res)=>{
+    res.render('./page/index2')
+});
+
+
+app.get('/formulario',(req,res)=>{
+    res.render('./page/formulario.ejs')})
+
+
 
 
 
